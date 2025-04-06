459d65d55119ed35518dba456e00bbeae5b0741c
JaagupA <jaagup.averin@gmail.com>
2025-04-06 19:20:38 +0300

    Update

--- scripts/west_commands.py
+++ scripts/west_commands.py
@@ -129,6 +129,7 @@ class GalePush(WestCommand):
             cmd = f"west forall -g gale -c '{subcmd}'"
             subprocess.check_call(cmd, shell=True)  # noqa: S602
             # Also repeat command for this (manifest) repository:
+            self.inf(f"Running {subcmd} in {file_dir}")
             subprocess.check_call(subcmd, shell=True, cwd=file_dir)  # noqa: S602
         except subprocess.CalledProcessError as e:
             self.wrn(f"Failed to commit and push changes: {e}")
